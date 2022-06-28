919c60a637946f055bd91470720ee91f694e06ca
AlexKur161 <78803205+AlexKur161@users.noreply.github.com>
2022-06-28 17:22:31 +0300

    new

--- todo.js
+++ todo.js
@@ -4,9 +4,8 @@ const btnShow = document.querySelector(".btn-inp");
 
 let tasks;
 let checkedBox;
-let arNo;
 
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 const rendering = (task,index) =>{
     return `
     <div class="task ${task.completed ? 'checked' : ''}">
@@ -22,36 +21,38 @@ const addingTask = () =>{
     showWrap.innerHTML = "";
     if(tasks.length > 0){
         tasks.forEach((item,index) => {
-            showWrap.innerHTML += rendering(item,index)
+            showWrap.innerHTML += rendering(item,index);
         })
         checkedBox = document.querySelectorAll('.task');
     }
 }
 addingTask();
 const updateLocal = () =>{
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 function Task(newTask){
     this.description = newTask;
-    this.completed = false
+    this.completed = false;
 }
 const completed = (index) => {
 tasks[index].completed = !tasks[index].completed;
 if(tasks[index].completed){
-checkedBox[index].classList.add('checked')
+checkedBox[index].classList.add('checked');
 }
 else if(!tasks[index].completed){
-    checkedBox[index].classList.remove('checked')
+    checkedBox[index].classList.remove('checked');
 }
 updateLocal(); 
 addingTask();
 }
 const deleteTask = (index) => {
-    console.log(tasks)
+    checkedBox[index].classList.add('showanimation');
+    setTimeout(() => {
     tasks.splice(index,1);
-    console.log(tasks)
     updateLocal(); 
     addingTask();
+    },1000)
+    
 }
 btnShow.addEventListener('click',()=> {
     tasks.push(new Task(inp.value));
